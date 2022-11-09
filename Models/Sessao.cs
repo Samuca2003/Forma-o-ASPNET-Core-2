65a0ed16ac4da35b39d17be1200dbd864004255e
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2022-11-09 09:57:40 -0300

    Add files via upload
    Essas pastas com classes fazem parte do projeto filmesAPI2

---
+++ Models/Sessao.cs
@@ -0,0 +1,27 @@
+﻿//using Castle.Components.DictionaryAdapter;
+using FilmesAPI.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FilmesApi.Models
+{
+    public class Sessao
+    {
+        [Key]
+        [Required]
+
+        public int Id { get; set; }
+        public virtual Cinema Cinema { get; set; }
+        public virtual Filme Filme { get; set; }
+        public int FilmeId { get; set; }
+        public int CinemaId { get; set; }
+        [JsonIgnore]
+        public DateTime HorarioDeEncerramento { get; set; }
+
+
+
+
+
+    }
+}
